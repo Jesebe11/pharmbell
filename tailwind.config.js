dbb568a2505bb03ca19f3c5c310176593447d37c
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-05-13 14:39:01 -0400

    Tailwind config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+
+      colors:{
+        'phr-blue-dark' : '#080e32',
+        'phr-blue-light' : '#2a96ff',
+        'phr-red' : '#E82121',
+        'phr-black' : '#111111',
+        'phr-white' : '#ffffff',
+        'phr-gray-dark' : '#696969',
+      },
+
+    },
   },
   variants: {},
   plugins: [],
