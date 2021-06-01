3566b5d2b197d9b44e0be05a8c0a399ff42c7875
Jesebell Escobar <jesebellinformatica.gmail.com>
2021-06-01 10:29:22 -0400

    change template guia and was configured colors base

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,13 @@ module.exports = {
     extend: {
 
       colors:{
-        'phr-blue-dark' : '#080e32',
-        'phr-blue-light' : '#2a96ff',
+        'phr-aqua-green' : '#37a794',
+        'phr-aqua-green-dark' : '#2a8071',
+        'phr-gray-dark' : '#262626',
+        'phr-gray-light' : '#a6a6a6',
         'phr-red' : '#E82121',
         'phr-black' : '#111111',
         'phr-white' : '#ffffff',
-        'phr-gray-dark' : '#696969',
       },
 
     },
